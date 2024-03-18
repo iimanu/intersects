839b1069a52cf35d55b5b4e7aba41f4113e3452b
Ido Imanuel <ido.imanuel@gmail.com>
2024-03-18 23:26:41 +0200

    Line width flag added as input to demo function

--- demo.py
+++ demo.py
@@ -10,15 +10,15 @@ from intersects.util import plot_uniform_image_montage, progress
 #
 # ----------------------------------------------------------------------------------------------------------------------
 
-def rectangle_montage_demo(n_images: int = 100, n_boxes_per_image: int = 2, image_size: Tuple[int, int] = (64, 64),
-                           are_square: bool = False, are_filled: bool = False, background_color='black'):
+def rectangle_montage_demo(n_images: int = 4, n_boxes_per_image: int = 40, image_size: Tuple[int, int] = (512, 512),
+                           are_square: bool = True, are_filled: bool = False, background_color='black', line_width=3):
     box_generator = RandomBoundingBoxGenerator(image_size=image_size, is_square=are_square)
     box_composer = BoundingBoxComposer()
     images, titles = [], []
     for i in range(n_images):
         bounding_boxes = [box_generator() for _ in range(n_boxes_per_image)]
         images.append(np.array(box_composer.compose(boxes=bounding_boxes, image_size=image_size, are_filled=are_filled,
-                                                    background_color=background_color, box_outline_width=1)))
+                                                    background_color=background_color, box_outline_width=line_width)))
         titles.append("X" if box_composer.are_intersecting(bounding_boxes) else "")
 
     plot_uniform_image_montage(images=images, title='Rectangle Montage', numbering=titles)
